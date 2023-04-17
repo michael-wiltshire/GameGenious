5ab8c75cd2db43f7b92d44980e3100276acdd042
Michael Wiltshire <mikulwinter@gmail.com>
2023-04-17 15:53:21 -0700

    successful data mapping

--- RetrieveData.py
+++ RetrieveData.py
@@ -1,6 +1,179 @@
 import requests
 import time
 
+
+all_champion_id = {
+        1: 'Annie',
+        2: 'Olaf',
+        3: 'Galio',
+        4: 'Twisted Fate',
+        5: 'Xin Zhao',
+        6: 'Urgot',
+        7: 'LeBlanc',
+        8: 'Vladimir',
+        9: 'Fiddlesticks',
+        10: 'Kayle',
+        11: 'Master Yi',
+        12: 'Alistar',
+        13: 'Ryze',
+        14: 'Sion',
+        15: 'Sivir',
+        16: 'Soraka',
+        17: 'Teemo',
+        18: 'Tristana',
+        19: 'Warwick',
+        20: 'Nunu & Willump',
+        21: 'Miss Fortune',
+        22: 'Ashe',
+        23: 'Tryndamere',
+        24: 'Jax',
+        25: 'Morgana',
+        26: 'Zilean',
+        27: 'Singed',
+        28: 'Evelynn',
+        29: 'Twitch',
+        30: 'Karthus',
+        31: "Cho'Gath",
+        32: 'Amumu',
+        33: 'Rammus',
+        34: 'Anivia',
+        35: 'Shaco',
+        36: 'Dr.Mundo',
+        37: 'Sona',
+        38: 'Kassadin',
+        39: 'Irelia',
+        40: 'Janna',
+        41: 'Gangplank',
+        42: 'Corki',
+        43: 'Karma',
+        44: 'Taric',
+        45: 'Veigar',
+        48: 'Trundle',
+        50: 'Swain',
+        51: 'Caitlyn',
+        53: 'Blitzcrank',
+        54: 'Malphite',
+        55: 'Katarina',
+        56: 'Nocturne',
+        57: 'Maokai',
+        58: 'Renekton',
+        59: 'JarvanIV',
+        60: 'Elise',
+        61: 'Orianna',
+        62: 'Wukong',
+        63: 'Brand',
+        64: 'LeeSin',
+        67: 'Vayne',
+        68: 'Rumble',
+        69: 'Cassiopeia',
+        72: 'Skarner',
+        74: 'Heimerdinger',
+        75: 'Nasus',
+        76: 'Nidalee',
+        77: 'Udyr',
+        78: 'Poppy',
+        79: 'Gragas',
+        80: 'Pantheon',
+        81: 'Ezreal',
+        82: 'Mordekaiser',
+        83: 'Yorick',
+        84: 'Akali',
+        85: 'Kennen',
+        86: 'Garen',
+        89: 'Leona',
+        90: 'Malzahar',
+        91: 'Talon',
+        92: 'Riven',
+        96: "Kog'Maw",
+        98: 'Shen',
+        99: 'Lux',
+        101: 'Xerath',
+        102: 'Shyvana',
+        103: 'Ahri',
+        104: 'Graves',
+        105: 'Fizz',
+        106: 'Volibear',
+        107: 'Rengar',
+        110: 'Varus',
+        111: 'Nautilus',
+        112: 'Viktor',
+        113: 'Sejuani',
+        114: 'Fiora',
+        115: 'Ziggs',
+        117: 'Lulu',
+        119: 'Draven',
+        120: 'Hecarim',
+        121: "Kha'Zix",
+        122: 'Darius',
+        126: 'Jayce',
+        127: 'Lissandra',
+        131: 'Diana',
+        133: 'Quinn',
+        134: 'Syndra',
+        136: 'AurelionSol',
+        141: 'Kayn',
+        142: 'Zoe',
+        143: 'Zyra',
+        145: "Kai'sa",
+        147: "Seraphine",
+        150: 'Gnar',
+        154: 'Zac',
+        157: 'Yasuo',
+        161: "Vel'Koz",
+        163: 'Taliyah',
+        166: "Akshan",
+        164: 'Camille',
+        201: 'Braum',
+        202: 'Jhin',
+        203: 'Kindred',
+        222: 'Jinx',
+        223: 'TahmKench',
+        234: 'Viego',
+        235: 'Senna',
+        236: 'Lucian',
+        238: 'Zed',
+        240: 'Kled',
+        245: 'Ekko',
+        246: 'Qiyana',
+        254: 'Vi',
+        266: 'Aatrox',
+        267: 'Nami',
+        268: 'Azir',
+        350: 'Yuumi',
+        360: 'Samira',
+        412: 'Thresh',
+        420: 'Illaoi',
+        421: "Rek'Sai",
+        427: 'Ivern',
+        429: 'Kalista',
+        432: 'Bard',
+        497: 'Rakan',
+        498: 'Xayah',
+        516: 'Ornn',
+        517: 'Sylas',
+        526: 'Rell',
+        518: 'Neeko',
+        523: 'Aphelios',
+        555: 'Pyke',
+        875: "Sett",
+        711: "Vex",
+        777: "Yone",
+        887: "Gwen",
+        876: "Lillia",
+        895: "Nilah",
+        360: "Samira",
+        147: "Seraphine",
+        526: "Rell",
+        234: "Viego",
+        887: "Gwen",
+        221: "Zeri",
+        888: "Renata Glasc",
+        902: "Milio",
+        897: "K'Sante",
+        200: "Bel'Veth",
+        711: "Vex",
+        166: "Akshan"
+    }
 # Enter your Riot Games API key here
 API_KEY = 'RGAPI-78c6851b-df8e-41ca-96e9-cd9ddcc61552'
 
@@ -11,20 +184,21 @@ summoner_name = 'Ignitos'
 url = f'https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/{summoner_id}?api_key={API_KEY}'
 
 # If the request was successful, print the champion being played by the player and the champions being played by the player's team
-while(True):
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        for participant in data['participants']:
-            if participant['summonerName'] == summoner_name:
-                print(f"You are playing as {participant['championName']}")
-                print("Your team consists of:")
-                for teammate in data['participants']:
-                    if teammate['teamId'] == participant['teamId']:
-                        print(teammate['championName'])
-    else:
-        print(f"Error: {response.status_code}")
-    time.sleep(5)
+
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+    for participant in data['participants']:
+        if participant['summonerName'] == summoner_name:
+            print(f"You are playing as {all_champion_id[participant['championId']]}")
+            print("Your team consists of:")
+            for teammate in data['participants']:
+                if teammate['teamId'] == participant['teamId']:
+                    print("team:",all_champion_id[teammate['championId']])
+                else:
+                    print("enemy:",all_champion_id[teammate['championId']])
+else:
+    print(f"Error: {response.status_code}")
 
 
 #1)Need to test Spectatev4 api and get info from request
